bc6097acc8e3efb11accf49ced471b788a8fdf52
shivraj0124 <shivrajkolwankar0124@gmail.com>
2026-08-01 00:49:42 +0530

    'api.ts-updated'

--- client/api.ts
+++ client/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
